4fff54c96c1b02c70daaa533a70132b0f91f04bd
jlavaty <35087240+jlavaty@users.noreply.github.com>
2018-02-21 18:58:37 -0600

    Add files via upload

---
+++ java/email.js
@@ -0,0 +1,8 @@
+$("#contact").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
+  });
+});
