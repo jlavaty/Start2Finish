cce07f2ce8d88f42a72811c7c2ca0d90dc00f7dd
jlavaty <35087240+jlavaty@users.noreply.github.com>
2018-02-26 18:08:47 -0600

    Add files via upload

---
+++ pictures/Home Page_files/scriptmenu.js
@@ -0,0 +1,15 @@
+var hamburger = document.querySelector('#hamburger');
+
+  var mNav = document.querySelector('.mobilenav');
+
+  hamburger.onclick = function(){
+
+    var status = mNav.classList.contains('active');
+    if(!status) {
+      mNav.classList.add('active');
+    } else {
+      mNav.classList.remove('active');
+    } 
+  
+  }
+
